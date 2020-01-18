bf5c49840a3397620b03e1d52921d1f7381e1e8c
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-01-18 14:32:45 -0800

    added test & added more info in readme

--- src/main/java/quotes/Quote.java
+++ src/main/java/quotes/Quote.java
@@ -6,6 +6,11 @@ public class Quote {
     String author;
     String text;
 
+    public Quote(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
 
     @Override
     public String toString() {
